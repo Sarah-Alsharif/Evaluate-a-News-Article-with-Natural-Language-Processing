3b729c5df5f2034aa70bee81c2bb65a18a33b206
Sarah <sarahAlsharif24@hotmail.com>
2020-06-25 19:17:53 +0300

    modfiy

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
 
@@ -71,6 +72,7 @@ module.exports = {
         cleanStaleWebpackAssets: true,
         protectWebpackAssets: false
 }),
+        new WorkboxPlugin.GenerateSW()
 ]
 
 }
